194e27824c2f939165be31ad06e3e2fd19eb2f56
Steven Patterosn <spatterson@aliceandsmith.com>
2016-01-26 17:08:42 -0500

    Added alt docs for CFO

--- stinger/stinger.command.implementations.js
+++ stinger/stinger.command.implementations.js
@@ -157,9 +157,10 @@
         command: 'stinger-ls',
         description: ['Diplays list of infected computer systems.'],
         handle: function (session) {
-            session.output.push({ output: true, text: ['morpho-2 <INFO: Stacy Holden, Morpho Medical VP Assistant>'], breakLine: true });
-            session.output.push({ output: true, text: ['morpho-1 <INFO: Greg Fields, Morpho Medical SysAdmin>'], breakLine: false });
-            session.output.push({ output: true, text: ['aconite-1 <INFO: Oran Plaskett, Aconite Capital CFO>'], breakLine: false });
+            session.output.push({ output: true, text: ['morpho-2\t << INFO: Stacy Holden, Morpho Medical VP Assistant (Office) >>\n\n'], breakLine: false });
+            session.output.push({ output: true, text: ['morpho-1\t << INFO: Greg Fields, Morpho Medical SysAdmin (Office) >>'], breakLine: false });
+            session.output.push({ output: true, text: ['aconite-1\t << INFO: Oran Plaskett, Aconite Capital CFO (Home) >>'], breakLine: false });
+            session.output.push({ output: true, text: ['\n>>STINGER INFECTED SYSTEMS<<'], breakLine: true });
         }
     });
 
@@ -227,9 +228,8 @@
             else if (param === "aconite-1 Documents") {
                 outText.push([
                     "\n>>STINGER DIRECTORY ACCESS GRANTED<<\n",
-                    "<"+ param + " Directory>\n", 
-                    "fiscalQ12014.doc",
-                    "fiscalQ42012.doc",
+                    "<"+ param + " Directory>\n",
+                    "Alone_Edgar_Allen_Poe.pdf",
                     "comcast_nov2015.pdf"
                     ].join("\n"));
             }
@@ -283,6 +283,33 @@
                     "One-time fees, PPV, Usage\t\t$0.00",
                     "Taxes, surcharges, fees\t\t\t$5.29",
                     "Total bill\t\t\t\t$83.22"
+                    ].join("\n"));
+            }
+            else if (param === "aconite-1 Alone_Edgar_Allen_Poe.pdf") {
+                outText.push([
+                    "\nSTINGER READ ACCESS <" + param + ">\n",
+                    "From childhood's hour I have not been",
+                    "As others were -- I have not seen",
+                    "As others saw -- I could not bring",
+                    "My passions from a common spring --",
+                    "From the same source I have not taken",
+                    "My sorrow -- I could not awaken",
+                    "My heart to joy at the same tone --",
+                    "And all I lov'd -- I lov'd alone --",
+                    "Then -- in my childhood -- in the dawn",
+                    "Of a most stormy life -- was drawn",
+                    "From ev'ry depth of good and ill",
+                    "The mystery which binds me still --",
+                    "From the torrent, or the fountain --",
+                    "From the red cliff of the mountain --",
+                    "From the sun that 'round me roll'd",
+                    "In its autumn tint of gold --",
+                    "From the lightning in the sky",
+                    "As it pass'd me flying by --",
+                    "From the thunder, and the storm --",
+                    "And the cloud that took the form",
+                    "(When the rest of Heaven was blue)",
+                    "Of a demon in my view --"
                     ].join("\n"));
             }
             else if (param === "aconite-1 reminders.txt") {
