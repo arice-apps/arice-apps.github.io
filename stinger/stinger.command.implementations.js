4811f63a706d79d856a15086f42303c2a4c078f0
Steven Patterosn <spatterson@aliceandsmith.com>
2016-01-27 16:43:12 -0500

    Added better formatted virus list

--- stinger/stinger.command.implementations.js
+++ stinger/stinger.command.implementations.js
@@ -382,7 +382,7 @@
         var me = {};
         me.command = 'stinger-exmal';
         me.description = ['Exploit and malware manager toolset, for use in cyberwarfare and cyberintel operations.',
-            "Example: stinger-exmal ExploitID MalwareName TargetID",
+            "Example: stinger-exmal ExploitID MalwareID TargetID",
             "See a list of malware available by typing 'stinger-exmal mals'",
             "See a list of exploits available by typing 'stinger-exmal exls'"];
         me.handle = function (session, param1, param2, param3) {
@@ -397,7 +397,7 @@
                 outText.push([
                     "\nSTINGER Malware List",
                     "========================",
-                    "TITLE",
+                    "MALWARE ID",
                     "OSX.RSPlug.A",
                     "OSX.HellRTS",
                     "OSX.Backloader",
